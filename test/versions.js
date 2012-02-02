d5f0edcd33c53fa8acfa04047939629b15fdac3c
James Halliday <mail@substack.net>
2012-02-02 02:03:57 -0800

    versioned semvers test passes

--- test/versions.js
+++ test/versions.js
@@ -14,41 +14,47 @@ test('versions', function (t) {
         seaport.connect(serverPort),
     ];
     
-    setTimeout(function () {
+    function onready () {
         var pending = 3;
         ports[0].get('beep', function (ps) {
             t.equal(ps.length, 3);
-            if (--pending === 3) t.end();
+            if (--pending === 0) t.end();
         });
         
         ports[0].get('beep@1.2.x', function (ps) {
             t.equal(ps.length, 1);
             t.equal(ps[0].port, ports_['beep@1.2.3']);
-            if (--pending === 3) t.end();
+            if (--pending === 0) t.end();
         });
         
         ports[0].get('beep@>1.2', function (ps) {
-            t.equal(ps.length, 1);
-            t.equal(ps[0].port, ports_['beep@1.3.5']);
-            if (--pending === 3) t.end();
+            t.equal(ps.length, 2);
+            t.deepEqual(
+                ps.map(function (p) { return p.port }).sort(),
+                [ ports_['beep@1.2.3'], ports_['beep@1.3.5'] ].sort()
+            );
+            
+            if (--pending === 0) t.end();
         });
-    }, 250);
+    }
     
     var ports_ = {};
-    setTimeout(function () {
-        ports[1].service('beep@1.2.3', function (port, ready) {
-            ports_['beep@1.2.3'] = port;
-            setTimeout(ready, 50);
-        });
-        ports[1].service('beep@1.3.5', function (port, ready) {
-            ports_['beep@1.3.5'] = port;
-            setTimeout(ready, 50);
-        });
-        ports[2].service('beep@0.9.2', function (port, ready) {
-            ports_['beep@0.9.2'] = port;
-            setTimeout(ready, 50);
-        });
-    }, 50);
+    var pending = 3;
+    ports[1].service('beep@1.2.3', function (port, ready) {
+        ports_['beep@1.2.3'] = port;
+        ready();
+        if (--pending === 0) setTimeout(onready, 50);
+    });
+    ports[1].service('beep@1.3.5', function (port, ready) {
+        ports_['beep@1.3.5'] = port;
+        ready();
+        if (--pending === 0) setTimeout(onready, 50);
+    });
+    ports[2].service('beep@0.9.2', function (port, ready) {
+        ports_['beep@0.9.2'] = port;
+        ready();
+        if (--pending === 0) setTimeout(onready, 50);
+    });
     
     t.on('end', function () {
         server.close();
